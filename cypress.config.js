a078e918cadde30f0efa6158d38a9a04b6a48b05
jefferson <jefferson.rlucia@gmail.com>
2026-02-21 18:30:47 -0300

    feat: add mochawesome test reporting and workflow improvements

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportFilename: 'report',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     specPattern: 'cypress/e2e/**/*.spec.js',
