b2a33bbdd6feacf90cda985ed48e0b81b1fbdf3b
jefferson <jefferson.rlucia@gmail.com>
2026-02-21 17:49:21 -0300

    chore: migrate from TypeScript to JavaScript

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    baseUrl: 'https://www.saucedemo.com',
-    specPattern: 'cypress/e2e/**/*.spec.js',
-    supportFile: 'cypress/support/e2e.js',
-  }
-})
--- cypress/support/commands.ts
+++
@@ -1,6 +0,0 @@
-// custom commands for Cypress
-Cypress.Commands.add('login', (username, password) => {
-  cy.get('[data-test="username"]').type(username)
-  cy.get('[data-test="password"]').type(password)
-  cy.get('[data-test="login-button"]').click()
-})
